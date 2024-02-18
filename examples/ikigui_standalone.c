b06946310df74af2df63da81a14fbae09b29b67f
logos-maker <100213740+logos-maker@users.noreply.github.com>
2024-02-18 13:47:05 +0100

    Add files via upload

--- examples/ikigui_standalone.c
+++ examples/ikigui_standalone.c
@@ -6,56 +6,32 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+#define IKIGUI_STANDALONE // define before we include ikigui.h
 #include "../libs/ikigui.h"
-//#include "font.h"
 #include "bmp.h"
-#define IKIGUI_STANDALONE
 
-#ifdef __linux__
-	#include <unistd.h>
-	void Sleep(int milisec){ usleep(milisec *1000); } //pass in microseconds
-#endif
-/*
 void main(){
-	ikigui_window mywin;
-	ikigui_window_open(&mywin,200,100);
+	char hello[13] = "HELLO WORLD!";
+	ikigui_window mywin ; // Create a struct for the window
 
-	while(1) { // look for events forever...
-	        ikigui_window_get_events(&mywin); // get the next event.  Note: only events we set the mask for are detected!
-		Sleep(34);
-                ikigui_window_update(&mywin);
-	}
-}
-*/
+	ikigui_image pic ;    // Create a struct for the image to load	
 
-void main () {
-        ikigui_image pic ;
-	ikigui_window mywin ; // create our variables/struct
-	ikigui_window_open(&mywin,1024,800); // Create window
-        ikigui_rect rect = {
-                .w= 8, .h=8, .x=1224, .y=0
-        };
+        ikigui_map menubar;   // Create a struct for the tile map to paint as text
+	ikigui_map_init(&menubar,&mywin.frame,&pic,OFFSET_ASCII,0,0,8,8,48,1); // init the setings for the tilemap used as text area
 
-        ikigui_include_bmp(&pic,bmp_array);
-	//ikigui_include_bmp(&pic,font_array);
-        
-        ikigui_map menubar; //ikigui_map_init(&menubar, &mywin.frame,&pic,48,1);// init... menu menubar
-	ikigui_map_init(&menubar,&mywin.frame,&pic,OFFSET_ASCII,0,0,8,8,48,1);
+	ikigui_include_bmp(&pic,bmp_array); // load the bmp image in the header file
+	ikigui_rect rect = { .w= 8, .h=8, .x=1224, .y=0 }; // selection of a part of the image to paint with mouse
 
-        //sprintf((char*)menubar.map,"| | FILE  | EDIT  | VIEW | TOOLS | SETTINGS | HELP |"); // Statusbar apperance
-        sprintf((char*)menubar.map,"FINE |");
-        ikigui_map_draw(&menubar,1,0,0);           // Draw menubar
+	ikigui_window_open(&mywin,1024,800); // Create and open window and specify window size
 
-	while(1) { // look for events forever...
-	        ikigui_window_get_events(&mywin); // get the next event.  Note: only events we set the mask for are detected!
-		if(mywin.mouse.buttons & MOUSE_LEFT){
-                        ikigui_blit_fast(&mywin.frame,&pic,mywin.mouse.x,mywin.mouse.y,&rect);
-                        //mywin.frame.pixels[mywin.mouse.x + mywin.mouse.y*mywin.frame.w] = 0xffffffff;
-                }
-                //if(mywin.mouse.buttons & MOUSE_LEFT) mywin.pixels[mywin.mouse.x + mywin.mouse.y*mywin.w] = 0x00ffffff;
-                sprintf((char*)menubar.map,"%04d",mywin.mouse.y);
-                ikigui_map_draw(&menubar,1,0,0);           // Draw menubar
-		Sleep(34);
-                ikigui_window_update(&mywin);
+	while(1) { // Event loop! look for events forever...
+	        ikigui_window_get_events(&mywin); // get the window events. Get events from mouse and window interactions. 
+		if(mywin.mouse.buttons & MOUSE_LEFT){ ikigui_blit_fast(&mywin.frame,&pic,mywin.mouse.x,mywin.mouse.y,&rect); } // blit image part to window if mouse down
+
+                sprintf((char*)menubar.map,"%04d",mywin.mouse.y); // Generate text bytes
+                ikigui_map_draw(&menubar,1,0,0);	// Draw menubar
+		ikigui_breathe(34);			// Pause to give CPU power over to the OS
+                ikigui_window_update(&mywin);		// update the window graphics
 	}
 }
