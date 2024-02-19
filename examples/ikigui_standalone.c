d46e12bd169d014220a68e85b11e8d6704cd6766
logos-maker <100213740+logos-maker@users.noreply.github.com>
2024-02-20 00:53:57 +0100

    Add files via upload

--- examples/ikigui_standalone.c
+++ examples/ikigui_standalone.c
@@ -3,10 +3,6 @@
 // for windows... (-mwindows gets rid of terminal opening)
 // gcc ikigui_standalone.c -o standalone.exe -lgdi32 -mwindows
 
-#include <stdint.h>
-#include <stdlib.h>
-#include <stdio.h>
-
 #define IKIGUI_STANDALONE // define before we include ikigui.h
 #include "../libs/ikigui.h"
 #include "bmp.h"
@@ -18,7 +14,7 @@ void main(){
 	ikigui_image pic ;    // Create a struct for the image to load	
 
         ikigui_map menubar;   // Create a struct for the tile map to paint as text
-	ikigui_map_init(&menubar,&mywin.frame,&pic,OFFSET_ASCII,0,0,8,8,48,1); // init the setings for the tilemap used as text area
+	ikigui_map_init(&menubar,&mywin.image,&pic,OFFSET_ASCII,0,0,8,8,48,1); // init the setings for the tilemap used as text area
 
 	ikigui_include_bmp(&pic,bmp_array); // load the bmp image in the header file
 	ikigui_rect rect = { .w= 8, .h=8, .x=1224, .y=0 }; // selection of a part of the image to paint with mouse
@@ -27,7 +23,7 @@ void main(){
 
 	while(1) { // Event loop! look for events forever...
 	        ikigui_window_get_events(&mywin); // get the window events. Get events from mouse and window interactions. 
-		if(mywin.mouse.buttons & MOUSE_LEFT){ ikigui_blit_fast(&mywin.frame,&pic,mywin.mouse.x,mywin.mouse.y,&rect); } // blit image part to window if mouse down
+		if(mywin.mouse.buttons & MOUSE_LEFT){ ikigui_blit_fast(&mywin.image,&pic,mywin.mouse.x,mywin.mouse.y,&rect); } // blit image part to window if mouse down
 
                 sprintf((char*)menubar.map,"%04d",mywin.mouse.y); // Generate text bytes
                 ikigui_map_draw(&menubar,1,0,0);	// Draw menubar
