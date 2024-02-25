64d19397a2af6e47933d3a50c5f41174d039eddc
logos-maker <100213740+logos-maker@users.noreply.github.com>
2024-02-25 18:38:37 +0100

    Add files via upload

---
+++ examples/stb_image/iki_stb_image.c
@@ -0,0 +1,18 @@
+#define IKIGUI_IMAGE
+#define IKIGUI_STANDALONE
+#include "../../libs/ikigui.h"
+#include <stdint.h>
+
+int main(void){
+
+    ikigui_window win;
+    ikigui_image img;
+    ikigui_image_load(&img,"./img/600x400.jpg");
+    ikigui_window_open(&win,img.w,img.h);
+    ikigui_draw_image(&win.image,&img,0,0);
+
+    while(1){
+	ikigui_window_till(&win,34);
+    }
+    return 0;
+}    
