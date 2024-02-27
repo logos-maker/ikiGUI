1a4bf803197c33aab8f1c6d8705b256a4a37adc2
logos-maker <100213740+logos-maker@users.noreply.github.com>
2024-02-27 18:00:00 +0100

    Add files via upload

---
+++ ikigui.h
@@ -0,0 +1,150 @@
+/// @file ikigui.h Main file for ikiGUI. Has drawing functions, for making tilemaps that select image parts from a tile atlas, for oldschool monospace characters and graphic tiles.
+
+/// For a rectangle with position and dimensions
+typedef struct{ 
+        int x; ///< x coordinate for the upper left position (in pixels)
+        int y; ///< y coordinate for the upper left position (in pixels)
+        int w; ///< width of the rectangle (in pixels)
+        int h; ///< hight of the rectangle (in pixels)
+} ikigui_rect;
+
+/// For a image with dimensions and pointer to pixel data
+typedef struct {
+	int w; ///< width of image (in pixels)
+	int h; ///< hight of image (in pixels)
+	unsigned int *pixels;   ///< pointer to pixel buffer in ARGB8888 format
+        unsigned int size;      ///< the size of the buffer
+        unsigned int color;  ///< color that may be used of tile map drawing for filling background
+	unsigned char composit; ///< flag for internal usage only (for compabillity reasons).
+} ikigui_image;
+#ifdef IKIGUI_STANDALONE
+	#include <stdint.h>
+	#include <stdlib.h>
+	#include <stdio.h>
+#endif
+#ifdef IKIGUI_TTF
+	#include "ikigui_ttf.h"
+#endif
+#ifdef IKIGUI_IMAGE
+	#include "ikigui_image.h"
+#endif
+#ifdef IKIGUI_DIAL
+	#include "ikigui_dial.h"
+#endif
+#ifndef IKIGUI_DRAW_ONLY // that declatation excludes all platform specific code, so it can be used for drawing into pixelbuffers only.
+#ifdef __linux__ //linux specific code goes here...
+	#include "ikigui_lin.h"	// For window and graphics handling in this case for Linux.
+#elif _WIN32 // windows specific code goes here...
+	#include "windows.h"
+	#include "ikigui_win.h" // For window and graphics handling in this case for Windows.
+#endif
+#endif
+#ifdef IKIGUI_DRAW_ONLY
+	#include "ikigui_regular.h" // we use regular as it's usual that the first pixel is in the top left corner.  
+#endif
+
+typedef uint32_t ikigui_color; /// An less unambiguous way to rembemer how to make a color variable and not color constants.
+
+/// to make a color by it's separate components
+uint32_t ikigui_color_make(uint8_t a, uint8_t r, uint8_t g, uint8_t b){ return (a << 24) + (r << 16) + (g << 8) + (b << 0) ;}
+
+/// For a tile map that can be drawn to images or windows
+typedef struct ikigui_map {
+   ikigui_image   *dest;       ///< The destination image to use for drawing the character display
+   ikigui_image   *source;     ///< The source image with graphics for tiles
+   unsigned char  columns;     ///< Number of columns in the character display
+   unsigned char  rows;        ///< Number of rows in the character display
+   unsigned char  tile_width;  ///< The pixel width of the characters/tiles in the source image
+   unsigned char  tile_hight;  ///< The pixel hight of the characters/tiles in the source image
+   char		  *map;	       ///< The start address for the character display
+   unsigned char  direction;   ///< The direction of the immages in the source image. Is autodetected by ikigui_map_init().
+   uint16_t       max_index;   ///< The number of tiles in the map - 1.
+   int		  x_spacing;   ///< The horizontal spacing between the left sides of the tiles in pixels.
+   int		  y_spacing;   ///< The vertical spacing between the top sides of the tiles in pixels.
+   signed char	  offset;      ///< The number offset for the numbers in the map. Can be used to fill it with ASCII text for example.
+} ikigui_map;
+
+enum offset {OFFSET_ASCII = -32, OFFSET_NORMAL = 0 }; // Used for setting the offset value in ikigui_map_inite later used by the ikigui_map_draw function.
+/// To initialize ikigui_map structs and allocate memory for the char map
+int ikigui_map_init(struct ikigui_map *display, ikigui_image *dest, ikigui_image *source,  int8_t offset, int x_spacing, int y_spacing, int width, int hight, int columns, int rows ){
+   display->map = (char*)calloc(columns*rows,sizeof(char));
+   display->offset = offset ;      // index value offset to all values used in the map array.
+   display->tile_width = width ;   // The width of the tiles. 
+   display->tile_hight = hight ;   // The higth of the tiles.
+   display->x_spacing = x_spacing ;// The number of pixels between left most pixel in each tile.
+   display->y_spacing = y_spacing ;// The number of pixels between top  most pixel in each tile.
+   if(x_spacing == 0) display->x_spacing = display->tile_width;  // if x_spacing is zero, set it to the size of the tile so they are placed side by side.
+   if(y_spacing == 0) display->y_spacing = display->tile_hight ; // if y_spacing is zero, set it to the size of the tile so they are placed side by side.
+   display->columns = columns ;    // Set to a default value
+   display->rows = rows ;          // Set to a default value
+   display->max_index = (source->w / width) * (source->h / hight) - 1 ; //
+   display->dest = dest ;          // Set to dest given as input by library user.
+   display->source = source ;      // Set to source image given as input by library user.
+   if(source->w == width) display->direction = 0; else display->direction = 1; // Automatic detection for tile-atlas direction (horizontal or vertical).
+   return columns * rows ; // returns the total number of tiles in the character display
+}
+
+/// To free memory allocated to a ikigui_map with the ikigui_map_init function
+void ikigui_map_free(struct ikigui_map *display){
+   free(display->map); // Free memory that was allocated with ikigui init.
+}
+/// To check if a coordinate is inside a specific ikigui rect
+int ikigui_mouse_pos_rect(ikigui_rect *box, int x, int y){ // is the x y coordinate inside the ikigui_rect
+	if(x<box->x) return 0;
+	if(x>(box->x+box->w)) return 0;
+	if(y<box->y) return 0;
+	if(y>(box->y+box->h)) return 0;
+	return -1; // Return a hit/true value.
+}
+// A helper function
+char* ikigui_map_layout(ikigui_map* find_text_pointer){ /// get the char* pointer to the array that is used for the tile/character array 
+	return find_text_pointer->map;
+}
+// A helper function
+void ikigui_rect_find(ikigui_rect *destin_rect, int x, int y, ikigui_rect *source_rect){ /// Fill in the parameters of the destin_rect. Convinience to automatically get the destination rect area for a hypthetical blit operation parameters.
+	destin_rect->x = x ;
+	destin_rect->y = y ;
+	destin_rect->w = source_rect->w ;
+	destin_rect->h = source_rect->h ; 
+}
+/// To give the index to a tile from a pixel coordinate
+int ikigui_mouse_pos_map(struct ikigui_map *display, int x, int y){ // returns -1 if outside the character display
+   if( (x < 0) || (y < 0) ) return -1;			// To the left or over the map
+   if( ( x < (( display->x_spacing) * display->columns))// To the right of the map 
+   &&  ( y < (( display->y_spacing) * display->rows))	// below the map 
+   ){  
+	int col = x / display->x_spacing ;
+	int row = y / display->y_spacing ;
+	if( (x < ( col * display->x_spacing  +display->tile_width)) && (y < ( row * display->y_spacing  +display->tile_hight)) ) // inside the tile
+	return (col + (row * display->columns)); // Returns the index of the character in your character array.
+   };
+   return -1; // Pixel coordinate is outside of the character display. 
+}
+
+enum tile_type { TILE_APLHA = 0, TILE_FILLED = 1, TILE_SOLID = 2, TILE_HOLLOW = 3 }; /// Blit modes/types for ikigui_map_draw()
+/// To draw a tile map to a window or image
+void ikigui_map_draw(struct ikigui_map *display, char tile_type, int x, int y){  // x y is pixel coordinate to draw it in the window
+   
+   ikigui_rect srcrect = { .w = display->tile_width, .h = display->tile_hight }; // , .x = 0, .y = 0,  } ;
+   ikigui_rect dstrect = { .w = display->tile_width, .h = display->tile_hight };
+
+   int set_w;
+   if(display->direction) set_w = srcrect.w ;
+   else                   set_w = srcrect.h ;
+
+   for(int i = 0 ; i < display->rows ; i++ ){	// draw all rows
+      dstrect.y = i * display->y_spacing + y;
+      for(int j = 0 ; j < display->columns ; j++ ){	// draw all columns
+	  dstrect.x = (j * display->x_spacing) + x;
+          int val = set_w * (display->map[i*display->columns + j] + display->offset) ;
+	  if(val<0) continue ; // Don't draw tile if given lower value than 0. // val=0; 
+	  if(display->direction)srcrect.x = val ; else srcrect.y = val ;
+	  switch(tile_type){ // Draw the character buffer to window.
+		  case 0:       ikigui_tile_alpha (display->dest,display->source, dstrect.x, dstrect.y, &srcrect);	break;
+		  case 1:	ikigui_tile_filled(display->dest,display->source, dstrect.x, dstrect.y, &srcrect);	break;
+		  case 2:	ikigui_tile_fast  (display->dest,display->source, dstrect.x, dstrect.y, &srcrect);	break;
+		  case 3:	ikigui_tile_hollow(display->dest,display->source, dstrect.x, dstrect.y, &srcrect);	break;
+	  }
+      }
+   }
+}
---
+++ ikigui_win.h
@@ -0,0 +1,182 @@
+/// @file ikigui_win.h Things that are platform specific to Windows (using GDI from the WIN32 API)
+// HINSTANCE hInstance; // https://stackoverflow.com/questions/15462064/hinstance-in-createwindow
+// #define PRINT_ERROR(a, args...) printf("ERROR %s() %s Line %d: " a, __FUNCTION__, __FILE__, __LINE__, ##args);
+
+// missing function: void ikigui_window_close(ikigui_window *mywin) // Not thought about it as it's done automatically by the DAW
+
+#include <windows.h>
+#include <stdbool.h>
+#include "ikigui_goofy.h" // Windows uses the goofy functions with the first pixel in the lower left in pixel buffers.
+
+bool quit = false;
+
+/// Is referenced and is a member of the window struct, and don't need to be declared separatly.
+struct mouse{
+	int x, y;
+	int x_intern, y_intern;
+	unsigned char buttons;
+	unsigned char buttons_intern;
+	int pressed ;
+	int old_x ;
+	int old_y ;
+	int old_button_press;
+	char left_click;  	// mouse down events
+	char middle_click;  	// mouse down events
+	char right_click; 	// mouse down events
+	char left_release;  	// mouse down events
+	char middle_release;  	// mouse down events
+	char right_release; 	// mouse down events
+	POINT pos;
+} ;
+
+/// A window struct, that can be used to open a window with the ikigui_open_plugin_window function.
+typedef struct ikigui_window{
+        ikigui_image image;
+        struct mouse mouse;
+        HWND window_handle;
+        BITMAPINFO bitmap_info;
+        HBITMAP bitmap;
+        HDC bitmap_device_context;
+        bool keyboard[256];
+        bool has_focus;
+        MSG message;     
+} ikigui_window; 
+
+int old_x;
+int old_y;
+
+
+enum { MOUSE_LEFT = 0b1, MOUSE_MIDDLE = 0b10, MOUSE_RIGHT = 0b100, MOUSE_X1 = 0b1000, MOUSE_X2 = 0b10000 };
+
+/// For internal usage only
+LRESULT CALLBACK WindowProcessMessage(HWND window_handle, UINT message, WPARAM wParam, LPARAM lParam) { 
+
+	ikigui_window *mywin = (ikigui_window*)GetWindowLongPtr(window_handle, GWLP_USERDATA);
+
+	switch(message) {
+		case WM_CLOSE: DestroyWindow(window_handle); break;// Someone has pressed x on window
+		case WM_QUIT:     break;// This function is usually called when the main window receives WM_DESTROY
+		case WM_DESTROY: DestroyWindow(window_handle); break;// Window is about to be destroyed, and can not be hindered.
+		case WM_PAINT: {
+			PAINTSTRUCT paint;
+			HDC device_context;
+			device_context = BeginPaint(window_handle, &paint);
+			BitBlt(device_context, paint.rcPaint.left, paint.rcPaint.top, paint.rcPaint.right - paint.rcPaint.left, paint.rcPaint.bottom - paint.rcPaint.top, mywin->bitmap_device_context, paint.rcPaint.left, paint.rcPaint.top, SRCCOPY);
+			EndPaint(window_handle,&paint);
+		} break;
+		case WM_KILLFOCUS: mywin->has_focus = false; break;
+		case WM_SETFOCUS: mywin->has_focus = true; break;
+
+		case WM_KEYDOWN: break;
+		case WM_KEYUP:   break;
+
+		case WM_MOUSEMOVE: { POINT pos;
+			if(mywin->mouse.buttons == MOUSE_LEFT){ // SetCursorPos();
+				GetCursorPos(&pos);				// absolute coordinate on screen
+				mywin->mouse.y_intern += pos.y - mywin->mouse.pos.y;
+				mywin->mouse.pos.y = pos.y;
+				mywin->mouse.x_intern += pos.x - mywin->mouse.pos.x;	
+				mywin->mouse.pos.x = pos.x;			
+			
+			}else{
+				mywin->mouse.x_intern = LOWORD(lParam);		// coordniate relative to window
+				mywin->mouse.y_intern = HIWORD(lParam);
+			}
+		} break;
+		case WM_MOUSELEAVE: break;
+		case WM_LBUTTONDOWN: mywin->mouse.buttons_intern |=  MOUSE_LEFT;   SetCapture(window_handle); GetCursorPos(&mywin->mouse.pos);break;
+		case WM_LBUTTONUP:   mywin->mouse.buttons_intern &= ~MOUSE_LEFT;   ReleaseCapture();break;
+		case WM_MBUTTONDOWN: mywin->mouse.buttons_intern |=  MOUSE_MIDDLE; break;
+		case WM_MBUTTONUP:   mywin->mouse.buttons_intern &= ~MOUSE_MIDDLE; break;
+		case WM_RBUTTONDOWN: mywin->mouse.buttons_intern |=  MOUSE_RIGHT;  break;
+		case WM_RBUTTONUP:   mywin->mouse.buttons_intern &= ~MOUSE_RIGHT;  break;
+
+		case WM_MOUSEWHEEL: {
+			// printf("%s\n", wParam & 0b10000000000000000000000000000000 ? "Down" : "Up");
+		} break;
+
+		default: return DefWindowProc(window_handle, message, wParam, lParam);
+	}
+	return 0;
+}
+
+/// A helper function that return the pointer to the ikigui_image inside a ikigui_window
+ikigui_image*	ikigui_image_of_window(ikigui_window* window){ return &window->image; }
+
+
+/// Open a child window (for a audio plugin window)
+void ikigui_window_open_editor(ikigui_window *mywin,void *ptr, int w, int h){
+
+	const wchar_t window_class_name[] = L"Window Class";
+	static WNDCLASS window_class = { 0 };
+
+	window_class.lpfnWndProc = WindowProcessMessage;
+        window_class.hInstance = GetModuleHandle(NULL);
+        window_class.hCursor = LoadCursor(NULL, IDC_ARROW);
+	window_class.lpszClassName = (PCSTR)window_class_name;
+        window_class.hbrBackground = (HBRUSH) GetStockObject (WHITE_BRUSH);
+        window_class.lpszMenuName = NULL;
+	RegisterClass(&window_class);
+
+        // To do before opening window!
+	mywin->bitmap_info.bmiHeader.biSize = sizeof(mywin->bitmap_info.bmiHeader);
+	mywin->bitmap_info.bmiHeader.biPlanes = 1;
+	mywin->bitmap_info.bmiHeader.biBitCount = 32;
+	mywin->bitmap_info.bmiHeader.biCompression = BI_RGB;
+	mywin->bitmap_device_context = CreateCompatibleDC(0);
+
+	if(ptr == NULL)mywin->window_handle = CreateWindow((PCSTR)window_class_name, "BitBlt Test Program ", WS_OVERLAPPEDWINDOW | WS_VISIBLE | WS_BORDER, CW_USEDEFAULT, CW_USEDEFAULT, w, h, NULL, NULL, GetModuleHandle(NULL), NULL);
+        else mywin->window_handle = CreateWindow((PCSTR)window_class_name, "BitBlt Test Program ", WS_CHILD | WS_VISIBLE , CW_USEDEFAULT, CW_USEDEFAULT, w, h, (HWND)ptr, NULL, NULL, NULL);
+	if(mywin->window_handle == NULL) {
+	//	PRINT_ERROR("CreateWindow() failed. Returned NULL.\n");
+	}
+
+	mywin->image.w = mywin->bitmap_info.bmiHeader.biWidth = w;
+	mywin->image.h = mywin->bitmap_info.bmiHeader.biHeight = h;
+	if(mywin->bitmap) DeleteObject(mywin->bitmap);
+	mywin->bitmap = CreateDIBSection(NULL, &mywin->bitmap_info, DIB_RGB_COLORS, (void**)&mywin->image.pixels, 0, 0);
+	SelectObject(mywin->bitmap_device_context, mywin->bitmap);
+	SetWindowLongPtr(mywin->window_handle, GWLP_USERDATA, (LONG_PTR)mywin);
+}
+
+/// Update the event data for the Window
+void ikigui_window_get_events(struct ikigui_window *mywin){
+
+
+        while(PeekMessage(&mywin->message, NULL, 0, 0, PM_REMOVE)) { DispatchMessage(&mywin->message); }
+
+        // values for recognicing changes in mousemovements and mouse buttons.
+        mywin->mouse.old_x = mywin->mouse.x ;     // old value for x coordinate.
+        mywin->mouse.old_y = mywin->mouse.y ;     // old value for y coodrinate.
+	mywin->mouse.x=mywin->mouse.x_intern;
+	mywin->mouse.y=mywin->mouse.y_intern;
+	mywin->mouse.old_button_press = mywin->mouse.buttons;  // old value for buttons. For finding changes later on.
+	mywin->mouse.buttons = mywin->mouse.buttons_intern; 
+	mywin->mouse.left_click   = (mywin->mouse.old_button_press == 0) && (mywin->mouse.buttons & MOUSE_LEFT);
+	mywin->mouse.middle_click = (mywin->mouse.old_button_press == 0) && (mywin->mouse.buttons & MOUSE_MIDDLE);
+	mywin->mouse.right_click  = (mywin->mouse.old_button_press == 0) && (mywin->mouse.buttons & MOUSE_RIGHT);
+	mywin->mouse.left_release   = (mywin->mouse.old_button_press == 1) && (!(mywin->mouse.buttons & MOUSE_LEFT));
+	mywin->mouse.middle_release = (mywin->mouse.old_button_press == 1) && (!(mywin->mouse.buttons & MOUSE_MIDDLE));
+	mywin->mouse.right_release  = (mywin->mouse.old_button_press == 1) && (!(mywin->mouse.buttons & MOUSE_RIGHT));
+
+}
+/// Updates the Window graphics with new things drawn
+void ikigui_window_update(struct ikigui_window *mywin){
+        InvalidateRect(mywin->window_handle, NULL, FALSE);
+        UpdateWindow(mywin->window_handle);
+}
+
+#ifdef IKIGUI_STANDALONE
+	void ikigui_breathe(int milisec){ Sleep(milisec); } // Pause function, gives CPU over to the OS
+
+	/// Open window (for a standalone application)
+	void ikigui_window_open(ikigui_window *mywin, int w, int h){
+		ikigui_window_open_editor(mywin,NULL, w, h);
+		mywin->mouse.buttons_intern = 0 ;
+	}
+	void ikigui_window_till(ikigui_window* win, int delay){ /// Helper for simplicity
+		ikigui_breathe(delay);
+		ikigui_window_get_events(win);
+		ikigui_window_update(win);
+	}
+#endif
