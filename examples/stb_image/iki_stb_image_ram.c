24e3d2dd39e5a42a4e48e55511323e5cc8023309
logos-maker <100213740+logos-maker@users.noreply.github.com>
2024-03-04 18:32:55 +0100

    Add files via upload

---
+++ examples/stb_image/iki_stb_image_ram.c
@@ -0,0 +1,19 @@
+#define IKIGUI_IMAGE
+#define IKIGUI_STANDALONE
+#include "../../libs/ikigui.h"
+#include "./img/image.h"
+#include <stdint.h>
+
+int main(void){
+
+    ikigui_window win;
+    ikigui_image img;
+    ikigui_include_image(&img,image_array,sizeof(image_array));
+    ikigui_window_open(&win, "Image from RAM", img.w, img.h);
+    ikigui_draw_image(&win.image,&img,0,0);
+
+    while(1){
+	ikigui_window_till(&win,34);
+    }
+    return 0;
+}    
